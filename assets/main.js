1313572f41d8e199b861f72f043cb6c87ab37444
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-17 17:18:26 +0200

    refactor: add function

--- assets/main.js
+++ assets/main.js
@@ -10,35 +10,32 @@ console.log('im alive');
 const ulElement = document.querySelector('ul');
 const buttonElement = document.querySelector('.button');
 
-// RICORDA: l'API logga un singolo indirizzo. Fai un loop * 10?
-for (let i = 0; i < 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then (response => {
-        console.log(response.data);
-        
-        // opzioni: crea markup qui o su html?
-        const liElement = document.createElement('li');
-        liElement.innerHTML = response.data.response;
-        ulElement.appendChild(liElement);
-    })
+// funzione per la chiamata api
+function apiCallLoop() {
+    for (let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                console.log(response.data);
+
+                // opzioni: crea markup qui o su html?
+                const liElement = document.createElement('li');
+                liElement.innerHTML = response.data.response;
+                ulElement.appendChild(liElement);
+            })
+    }
 }
 
+// richiamo funzione
+apiCallLoop();
+
+// RICORDA: l'API logga un singolo indirizzo. Fai un loop * 10?
+
 // per sezione bonus: appendi il for ad un bottone con add event listener?
-buttonElement.addEventListener('click', function() {
-    
+buttonElement.addEventListener('click', function () {
+
     // risvuotalo ad ogni click
     // muovere la costante fuori rende una sola mail
     ulElement.innerHTML = '';
 
-    for (let i = 0; i < 10; i++) {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (response => {
-            console.log(response.data);
-            
-            // opzioni: crea markup qui o su html?
-            const liElement = document.createElement('li');
-            liElement.innerHTML = response.data.response;
-            ulElement.appendChild(liElement);
-        })
-    }
+    apiCallLoop();
 })
