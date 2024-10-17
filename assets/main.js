e352d939e2e49f926c8a2e2e7cd0af5cb4d84335
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-17 15:18:55 +0200

    docs: fill html, try with for loop

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,20 @@
 console.log('im alive');
 
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then (response => {
-    console.log(response.data);
-})
+// Descrizione
+// Attraverso l'apposita API di Boolean generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Abbellire con CSS o Bootstrap
+// Inserire un bottone che al click fetchi altre 10 mail (sostituendo le altre)
+
+// dichiarazione variabili
+let listElement = document.querySelector('ul');
 
 // RICORDA: l'API logga un singolo indirizzo. Fai un loop * 10?
+for (let i = 0; i <= 10; i++) {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then (response => {
+        console.log(response.data);
+    })
+    
+    // opzioni: crea markup qui o su html?
+}
