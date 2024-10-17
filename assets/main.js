83f5ffd49bd967f17d374ad36161b2c693d02dc3
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-17 15:48:09 +0200

    docs: print try

--- assets/main.js
+++ assets/main.js
@@ -7,14 +7,17 @@ console.log('im alive');
 // Inserire un bottone che al click fetchi altre 10 mail (sostituendo le altre)
 
 // dichiarazione variabili
-let listElement = document.querySelector('ul');
+let ulElement = document.querySelector('ul');
 
 // RICORDA: l'API logga un singolo indirizzo. Fai un loop * 10?
 for (let i = 0; i <= 10; i++) {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then (response => {
         console.log(response.data);
+        
+        // opzioni: crea markup qui o su html?
+        let liElement = document.createElement('li');
+        liElement.innerHTML = response.data;
+        ulElement.appendChild(liElement);
     })
-    
-    // opzioni: crea markup qui o su html?
 }
