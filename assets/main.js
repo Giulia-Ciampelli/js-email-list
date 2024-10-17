c05386ebfc450170dc0f6e134b87e82587d0d3a5
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-17 16:13:28 +0200

    docs: add event listener to button

--- assets/main.js
+++ assets/main.js
@@ -7,7 +7,8 @@ console.log('im alive');
 // Inserire un bottone che al click fetchi altre 10 mail (sostituendo le altre)
 
 // dichiarazione variabili
-let ulElement = document.querySelector('ul');
+const ulElement = document.querySelector('ul');
+const buttonElement = document.querySelector('.button');
 
 // RICORDA: l'API logga un singolo indirizzo. Fai un loop * 10?
 for (let i = 0; i < 10; i++) {
@@ -16,10 +17,25 @@ for (let i = 0; i < 10; i++) {
         console.log(response.data);
         
         // opzioni: crea markup qui o su html?
-        let liElement = document.createElement('li');
+        const liElement = document.createElement('li');
         liElement.innerHTML = response.data.response;
         ulElement.appendChild(liElement);
     })
 }
 
 // per sezione bonus: appendi il for ad un bottone con add event listener?
+buttonElement.addEventListener('click', function() {
+    
+    
+    for (let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then (response => {
+            console.log(response.data);
+            
+            // opzioni: crea markup qui o su html?
+            const liElement = document.createElement('li');
+            liElement.innerHTML = response.data.response;
+            ulElement.appendChild(liElement);
+        })
+    }
+})
