577b60529065409eba0eb188daafb9e5bf58c380
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-17 15:12:33 +0200

    docs: api test

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,8 @@
 console.log('im alive');
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+.then (response => {
+    console.log(response.data);
+})
+
+// RICORDA: l'API logga un singolo indirizzo. Fai un loop * 10?
